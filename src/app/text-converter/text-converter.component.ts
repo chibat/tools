7b7c3b10087c81280ab35b7c2d96b9298deb68c9
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2018-05-09 12:50:22 +0900

    -

--- src/app/text-converter/text-converter.component.ts
+++ src/app/text-converter/text-converter.component.ts
@@ -8,19 +8,27 @@ import { FormControl } from '@angular/forms';
 })
 export class TextConverterComponent implements OnInit {
 
-  text = new FormControl(
+  text = new FormControl();
+
+  readonly snakeExample =
 `aaa_bbb_ccc
 AAA_BBB_CCC
-`
-  );
+`;
 
-  constructor() { }
+  readonly camelExample = 
+`aaaBbbCcc
+aaaBbbCcc
+`;
+
+  constructor() {
+    this.text.setValue(this.camelExample);
+  }
 
   ngOnInit() {
   }
 
   convert() {
-    this.text.setValue(this.snakeToCamel(this.text.value));
+    this.text.setValue(this.camelToSnake(this.text.value));
   }
 
   private snakeToCamel(text: string){
@@ -33,9 +41,9 @@ AAA_BBB_CCC
 
   private camelToSnake(text: string){
     return text.replace(/([A-Z])/g,
-      function(s) {
-        return '_' + s.charAt(0).toLowerCase();
+      function(s, p1, offset) {
+        return '_' + s.charAt(0);
       }
-    );
+    ).toLowerCase();
   }
 }
