11ff9f2088f9e9ec7da920ff75d36d8a70a7f1af
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2018-05-16 12:45:39 +0900

    -

--- src/app/text-converter/text-converter.component.ts
+++ src/app/text-converter/text-converter.component.ts
@@ -35,6 +35,17 @@ AAA_BBB_CCC
 `aaaBbbCcc
 aaaBbbCcc
 `
+    },
+    {
+      id: 2,
+      name: 'csv format',
+      convert: this.formatCsv,
+      example:
+`header1,header2,   header3
+1,2,    3
+11,22,    33
+1234567, 1, 2
+12345678, 1, 2`
     }
   ];
 
@@ -53,5 +64,42 @@ aaaBbbCcc
 
   convert() {
     this.text.setValue(this.actions[this.actionControl.value].convert(this.text.value));
+  }
+
+  private formatCsv(text: string) {
+    const lines = text.split(/\r\n|\r|\n/);
+    let formatted = '';
+    const headerIndexes = [];
+    for (let rowIndex = 0; rowIndex < lines.length; rowIndex++) {
+      if (rowIndex == 0) {
+        const line = lines[rowIndex].replace(/, */g, ', ').trim();
+        for (let fromIndex = 0; ; fromIndex++) {
+          fromIndex = line.indexOf(', ', fromIndex);
+          if (fromIndex === -1) {
+            break;
+          }
+          headerIndexes.push(fromIndex);
+        }
+        formatted += line;
+      } else {
+        formatted += "\n";
+        let line = '';
+        const columns = lines[rowIndex].split(',');
+        for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+          line += columns[columnIndex].trim();
+          if (columnIndex != columns.length - 1) {
+            line += ', '
+          }
+          if (headerIndexes.length > columnIndex) {
+            const spaceSize = headerIndexes[columnIndex] - line.length + 2;
+            for (let i = 0; i < spaceSize; i++) {
+              line += ' ';
+            }
+	  }
+        }
+        formatted += line.trim();
+      }
+    }
+    return formatted;
   }
 }
