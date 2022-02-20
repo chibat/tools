9186198f98e54d5c2bdbbdb16ca3c5406c1105bf
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2022-02-20 15:51:46 +0900

    deploy

--- aleph.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "https://deno.land/x/aleph@v0.3.0-alpha.33/types.ts";
-
-export default (): Config => ({
-  basePath: "/tool/",
-  outputDir: "docs",
-});
--- tasks.ts
+++ tasks.ts
@@ -1,62 +1,24 @@
 #!/usr/bin/env -S deno run -A
 
-import { existsSync } from "https://deno.land/std@0.93.0/fs/exists.ts";
-
 //
 // Tasks
 //
 
-const TASKS = [build, dev, start, deploy];
-
-async function build() {
-  rm(".aleph");
-  await denoRun([
-    "--allow-net=deno.land,esm.sh,cdn.esm.sh,registry.npmjs.org,cdn.jsdelivr.net",
-    "--allow-read=.," + Deno.execPath() + "," + await denoDir(),
-    "--allow-write",
-    "--allow-env=ALEPH_DEV,ALEPH_DEV_PORT,ALEPH_VERSION,ALEPH_BUILD_MODE,ALEPH_FRAMEWORK,ESBUILD_BINARY_PATH,XDG_CACHE_HOME,HOME,LOCALAPPDATA,DENO_TESTING",
-    alephCliPath(),
-    "build",
-  ]);
-}
+const TASKS = [start, deploy];
 
 async function deploy() {
-  await build();
-  Deno.createSync("docs/.nojekyll");
   await $("git add .");
   await $("git commit -m deploy");
   await $("git push");
 }
 
-async function dev() {
-  await denoRun([
-    "--allow-net=deno.land,esm.sh,cdn.esm.sh,:8080",
-    "--allow-read=.," + Deno.execPath() + "," + await denoDir(),
-    "--allow-write=.aleph," + await denoDir(),
-    "--allow-env=ALEPH_DEV,ALEPH_DEV_PORT,ALEPH_VERSION,ALEPH_BUILD_MODE,ALEPH_FRAMEWORK,DENO_TESTING",
-    alephCliPath(),
-    "dev",
-  ]);
-}
-
 async function start() {
   await denoRun([
-    "--allow-net=deno.land,esm.sh,:8080",
-    "--allow-read=.," + Deno.execPath() + "," + await denoDir(),
-    "--allow-write=.aleph," + await denoDir(),
-    "--allow-env=ALEPH_DEV,ALEPH_DEV_PORT,ALEPH_VERSION,ALEPH_BUILD_MODE,ALEPH_FRAMEWORK",
-    alephCliPath(),
-    "start",
+    "--allow-net=:4507",
+    "--allow-read",
+    "https://deno.land/std@0.106.0/http/file_server.ts",
+    "docs",
   ]);
-}
-
-//
-// Aleph.js
-//
-
-function alephCliPath() {
-  return JSON.parse(Deno.readTextFileSync("import_map.json"))
-    .imports["aleph/"] + "cli.ts";
 }
 
 //
@@ -79,23 +41,6 @@ async function denoRun(params: string[]) {
   await $(
     [Deno.execPath(), "run", "--allow-run"].concat(params),
   );
-}
-
-function rm(path: string) {
-  if (existsSync(path)) {
-    Deno.removeSync(path, { recursive: true });
-  }
-}
-
-async function denoDir() {
-  const p = Deno.run({
-    cmd: [Deno.execPath(), "info", "--json", "--unstable"],
-    stdout: "piped",
-    stderr: "null",
-  });
-  const output = (new TextDecoder()).decode(await p.output());
-  p.close();
-  return JSON.parse(output).denoDir;
 }
 
 //
